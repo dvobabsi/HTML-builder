967a173a253c57bad3c4e770f86a52b4e83c4881
dvobabsi <dvobabsi@gmail.com>
2023-05-09 02:48:47 +0300

    feat: add solution Task 4

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,17 +1,17 @@
 const fs = require('node:fs/promises');
 const path = require('path');
 
-const folder = path.resolve(__dirname, 'secret-folder');
+const pathToFolder = path.resolve(__dirname, 'secret-folder');
 
 async function getStat() {
   try {
-    const files = await fs.readdir(folder, { withFileTypes: true });
+    const files = await fs.readdir(pathToFolder, { withFileTypes: true });
     
     for (const file of files) {
       if (file.isFile()) {
         const name = path.parse(file.name).name;
         const ext = path.parse(file.name).ext.replace('.', '');
-        const info = await fs.stat(path.join(folder, file.name));
+        const info = await fs.stat(path.join(pathToFolder, file.name));
         console.log('\x1b[37m', name, '-', ext, '-', formatSize(info.size));
       }
     }
@@ -22,16 +22,16 @@ async function getStat() {
 
 function formatSize(size) {
   const unit = ['byte', 'KiB', 'MiB', 'GiB'];
-  let i = 0;
+  let index = 0;
   size = Number(size);
   for (let i = 0; i < unit.length - 1; i++) {
     if (size >= 1024) {
       size /= 1024;
-      i++;
+      index++;
     }
   }
-  size = unit[i] === 'byte' ? size : size.toFixed(3);
-  return size + unit[i];
+  size = unit[index] === 'byte' ? size : size.toFixed(3);
+  return size + unit[index];
 }
 
 getStat();
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,41 @@
+const fs = require('node:fs/promises');
+const path = require('path');
 
+const folder = 'files';
+const newFolder = 'files-copy';
+
+async function copyDir(source, dest) {
+
+  try {
+    await fs.rm(dest, { recursive: true, force: true});
+    await fs.mkdir(dest, { recursive: true });
+  } catch (error) {
+    console.error('Error creating directory:', error);
+    return;
+  }
+
+  const files = await fs.readdir(source);
+
+  for (const file of files) {
+
+    const sourceFile = path.join(source, file);
+    const destFile = path.join(dest, file);
+    const fileStat = await fs.stat(sourceFile);
+
+    if (fileStat.isDirectory()) {
+
+      await copyDir(sourceFile, destFile);
+
+    } else {
+
+      const content = await fs.readFile(sourceFile);
+      await fs.writeFile(destFile, content);
+      console.log(`${destFile} скопировано.`);
+    }
+  }
+}
+
+const source = path.join(__dirname, folder);
+const dest = path.join(__dirname, newFolder);
+
+copyDir(source, dest);
